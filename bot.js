3f3de236930671a3401226eea6a466bbd87bf868
Sobhix1 <47393468+Sobhix1@users.noreply.github.com>
2019-02-15 23:29:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` عمو عمو عمو `,"http://twitch.tv/Death Shop")
+client.user.setGame(`  VIP  `,"http://twitch.tv/Death Shop")
 client.user.setStatus("Online")
 });
 
