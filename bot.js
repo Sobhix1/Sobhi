c954a5165e916dd7618f11a70585e947bff95650
Sobhix1 <47393468+Sobhix1@users.noreply.github.com>
2019-02-16 17:23:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`  إذا كل الناس صارت في غابة إنت مش ضروري أبد أنك تصير حيوان :D  `,"http://twitch.tv/Death Shop")
+client.user.setGame(`  Business To Business  `,"http://twitch.tv/Death Shop")
 client.user.setStatus("Online")
 });
 
